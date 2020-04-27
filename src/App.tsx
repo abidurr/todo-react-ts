f1eb308e02ee6b7d8e43bffe858f3b70b507cf89
abidurr <42abid@gmail.com>
2020-04-27 00:49:46 -0400

    Starting again from scratch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const App : React.FC = () => {
+const todos = []
+
+const App = () => {
   return (
     <div>
       <h1> To-Do App</h1>
+      <TodoAdd />
+      <TodoItem todo={todos[0]}/>
+
+      
     </div>
   )
 }
