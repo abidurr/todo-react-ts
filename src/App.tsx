bbda0b507d47912dea4977a2098d76893f13a9c8
abidurr <42abid@gmail.com>
2020-04-27 00:05:01 -0400

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React from 'react'
 
+const App : React.FC = () => {
+  return (
+    <div>
+      <h1> To-Do App</h1>
+    </div>
+  )
+}
 
-export default App;
+export default App
